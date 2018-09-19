972195be91995a830932c4d7a115ed2a3e2d84c9
DeuterGraves <puckles@gmail.com>
2018-09-19 17:09:35 +0100

    extensions done yay

--- pub.rb
+++ pub.rb
@@ -15,9 +15,7 @@ class Pub
 
   def sells_item(drink, customer)
     increase_till(drink)
-    #p customer.wallet
     customer.decrease_wallet(drink)
-    #@wallet.decrease_wallet(drink)
   end
 
   def age_check?(customer)
@@ -26,21 +24,30 @@ class Pub
   end
 
   def drink_sale(drink, customer)
-    #if age check == true
     if age_check?(customer) == true
-    # increase_till
-    sells_item(drink, customer)
-    #decrease_wallet
-  else
-    return "computer says no"
+      sells_item(drink, customer)
+    else
+      return "computer says no"
+    end
   end
 
-# #write the function for the drunk check
-# def drunk_check(customer)
-#   #similar to age check - drinks max 5 - if 6+ = false
-#   #if 0-5 - true
-# end
+  def drunk_check?(customer)
+    drunk = customer.drunkenness
+    drunk < 5
+    #if drunk is less than 5 - customer is not drunk - true - safe to sell drink
+    # if this evaluates to false - then don't sell them a drink
+  end
 
+  def drunk_drink_sale(drink, customer)
+    #if drunk check = true
+    if drunk_check?(customer) == true
+      sells_item(drink, customer)
+    else
+      return "you're too drunk, mate"
+      #sell drink to customer
+      # if drunk check = false
+      #don't drink to customer
+    end
   end
 
 
--- specs/pub_spec.rb
+++ specs/pub_spec.rb
@@ -6,70 +6,93 @@ require_relative("../pub.rb")
 
 class PubTest < MiniTest::Test
 
-def setup()
-  @drink1 = Drink.new("Harvey Wallbanger", 3, 1)
-  @drink2 = Drink.new("Orchard Pigs Cider", 2, 1)
-  @drink3 = Drink.new("Tennants", 1, 1)
+  def setup()
+    @drink1 = Drink.new("Harvey Wallbanger", 3, 1)
+    @drink2 = Drink.new("Orchard Pigs Cider", 2, 1)
+    @drink3 = Drink.new("Tennants", 1, 1)
 
-  @drinks = [@drink1, @drink2, @drink3]
+    @drinks = [@drink1, @drink2, @drink3]
 
-  @customer1 = Customer.new("Richard", 50, 24)
-  @customer2 = Customer.new("Susan", 50, 17)
+    @customer1 = Customer.new("Richard", 50, 24)
+    @customer2 = Customer.new("Susan", 50, 17)
 
-  @pub1 = Pub.new("Filthy McNasties", 0, @drinks)
-end
+    @pub1 = Pub.new("Filthy McNasties", 0, @drinks)
+  end
 
-def test_pub_has_name()
-  assert_equal("Filthy McNasties", @pub1.name)
-end
+  def test_pub_has_name()
+    assert_equal("Filthy McNasties", @pub1.name)
+  end
 
-def test_pub_has_till()
-  assert_equal(0, @pub1.till)
-end
+  def test_pub_has_till()
+    assert_equal(0, @pub1.till)
+  end
 
-def test_pub_has_drinks()
-  assert_equal(@drinks, @pub1.drinks)
-end
+  def test_pub_has_drinks()
+    assert_equal(@drinks, @pub1.drinks)
+  end
 
-def test_increase_till()
-  @pub1.increase_till(@drink1)
-  assert_equal(3, @pub1.till)
-end
+  def test_increase_till()
+    @pub1.increase_till(@drink1)
+    assert_equal(3, @pub1.till)
+  end
 
-def test_sells_items()
-  @pub1.sells_item(@drink1, @customer1)
-  # test increase till
-  assert_equal(3, @pub1.till)
-  # test decrease wallet
-  assert_equal(47, @customer1.wallet)
-end
+  def test_sells_items()
+    @pub1.sells_item(@drink1, @customer1)
+    # test increase till
+    assert_equal(3, @pub1.till)
+    # test decrease wallet
+    assert_equal(47, @customer1.wallet)
+  end
 
-def test_check_age__of_age()
-  assert_equal(true, @pub1.age_check?(@customer1))
-end
+  def test_check_age__of_age()
+    assert_equal(true, @pub1.age_check?(@customer1))
+  end
 
-def test_check_age__not_of_age()
-  assert_equal(false, @pub1.age_check?(@customer2))
-end
+  def test_check_age__not_of_age()
+    assert_equal(false, @pub1.age_check?(@customer2))
+  end
 
-def test_age_check_drink_sale__of_age()
-  @pub1.drink_sale(@drink1, @customer1)
-  assert_equal(3, @pub1.till)
-  assert_equal(47, @customer1.wallet)
-end
+  def test_age_check_drink_sale__of_age()
+    @pub1.drink_sale(@drink1, @customer1)
+    assert_equal(3, @pub1.till)
+    assert_equal(47, @customer1.wallet)
+  end
 
-def test_age_check_drink_sale__not_of_age()
-  sale = @pub1.drink_sale(@drink1, @customer2)
-  assert_equal("computer says no", sale)
-end
+  def test_age_check_drink_sale__not_of_age()
+    sale = @pub1.drink_sale(@drink1, @customer2)
+    assert_equal("computer says no", sale)
+  end
 
-# def test_drunk_check_sale__not_drunk()
-#   assert_equal(true, @pub1.drunk_check(@customer1))
-# end
+  def test_drunk_check__not_drunk()
+    assert_equal(true, @pub1.drunk_check?(@customer1))
+  end
 
-# def test_drunk_check_sale__drunk()
-#   assert_equal(false, @pub1.drunk_check(@customer1))
-# end
+  def test_drunk_check__drunk()
+    @customer1.drinks(@drink1)
+    @customer1.drinks(@drink1)
+    @customer1.drinks(@drink1)
+    @customer1.drinks(@drink1)
+    @customer1.drinks(@drink1)
+    @customer1.drinks(@drink1)
+    assert_equal(false, @pub1.drunk_check?(@customer1))
+  end
 
+  def test_drunk_check_sale__not_drunk_enough()
+    @pub1.drunk_drink_sale(@drink1, @customer1)
+    assert_equal(3, @pub1.till)
+    assert_equal(47, @customer1.wallet)
+  end
+
+  def test_drunk_check_sale__too_drunk()
+    @customer1.drinks(@drink1)
+    @customer1.drinks(@drink1)
+    @customer1.drinks(@drink1)
+    @customer1.drinks(@drink1)
+    @customer1.drinks(@drink1)
+    @customer1.drinks(@drink1)
+    sale= @pub1.drunk_drink_sale(@drink1, @customer1)
+    assert_equal("you're too drunk, mate", sale)
+
+  end
   #end class
 end
