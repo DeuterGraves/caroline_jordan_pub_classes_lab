15b6f8f44fef947d6b5234abe6d49ee984f3def3
DeuterGraves <puckles@gmail.com>
2018-09-19 11:51:50 +0100

    built pub, passes basic tests! yay

---
+++ specs/pub_spec.rb
@@ -0,0 +1,32 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../drink.rb")
+require_relative("../customer.rb")
+require_relative("../pub.rb")
+
+class PubTest < MiniTest::Test
+
+def setup()
+  drink1 = Drink.new("Harvey Wallbanger", 3)
+  drink2 = Drink.new("Orchard Pigs Cider", 2)
+  drink3 = Drink.new("Tennants", 1)
+
+  @drinks = [drink1, drink2, drink3]
+
+  @pub1 = Pub.new("Filthy McNasties", 0, @drinks)
+end
+
+def test_pub_has_name()
+  assert_equal("Filthy McNasties", @pub1.name)
+end
+
+def test_pub_has_till()
+  assert_equal(0, @pub1.till)
+end
+
+def test_pub_has_drinks()
+  assert_equal(@drinks, @pub1.drinks)
+end
+
+  #end class
+end
